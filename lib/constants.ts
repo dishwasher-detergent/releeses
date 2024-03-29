5cc5faa08ddd1db5eac080e07305887fd62b2f77
Kenneth Bass <kenny_bass@outlook.com>
2024-03-29 16:57:05 -0500

    fix domain

--- lib/constants.ts
+++
@@ -1,5 +0,0 @@
-export const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN || "http://localhost:3000";
-export const ENDPOINT =
-  process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "https://cloud.appwrite.io/v1";
-export const PROJECT_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string;
-export const DATABASE_ID = process.env.NEXT_PUBLIC_DATABASE_ID as string;
