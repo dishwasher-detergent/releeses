7df7bf591f90e5032db8aa76c4725578a90e7ae3
Kenneth Bass <kenny_bass@outlook.com>
2024-03-29 18:00:42 -0500

    fix sign in

--- app/app/(auth)/signin/page.tsx
+++ app/app/(auth)/signin/page.tsx
@@ -1,16 +1,36 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { createClient } from "@/lib/supabase/client";
+import {
+  LucideGitBranch,
+  LucideGithub,
+  LucideGitlab,
+  LucideRocket,
+} from "lucide-react";
+import Link from "next/link";
 
 export default function SignInPage() {
   return (
     <Card className="mx-auto w-full max-w-sm p-4">
-      <CardHeader>
-        <CardTitle className="text-2xl">Sign In</CardTitle>
+      <CardHeader className="mb-4 border-b">
+        <Link href="/" className="mb-4 flex items-center gap-2 font-semibold">
+          <LucideRocket className="size-8 rounded-lg bg-foreground p-2 text-background" />
+          <span className="text-xl font-black">Releaser</span>
+        </Link>
+        <CardTitle className="text-lg">Sign In</CardTitle>
+        <CardDescription>
+          Use one of our 3rd party providers to sign in.
+        </CardDescription>
       </CardHeader>
-      <CardContent>
+      <CardContent className="flex flex-col gap-2">
         <Button
           className="w-full"
           onClick={async () => {
@@ -29,7 +49,16 @@ export default function SignInPage() {
             });
           }}
         >
-          Github
+          <LucideGithub className="mr-2 size-4" />
+          GitHub
+        </Button>
+        <Button disabled className="w-full">
+          <LucideGitBranch className="mr-2 size-4" />
+          BitBucket
+        </Button>
+        <Button disabled className="w-full">
+          <LucideGitlab className="mr-2 size-4" />
+          GitLab
         </Button>
       </CardContent>
     </Card>
