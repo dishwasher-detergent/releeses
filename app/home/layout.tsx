1bc8a64478b0e0e303a3d2b97b77ce66088618a0
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2024-11-13 14:22:26 -0600

    wider

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -8,11 +8,10 @@ export default function HomeLayout({
 }) {
   return (
     <main className="relative flex min-h-screen flex-col bg-black">
-      <Nav />
       <div className="relative z-10 mx-auto max-w-7xl py-4">{children}</div>
       <Footer />
       <div className="space-between absolute inset-0 z-0 h-full">
-        <div className="mx-auto flex h-full max-w-5xl flex-row flex-nowrap justify-between px-4">
+        <div className="mx-auto flex h-full max-w-7xl flex-row flex-nowrap justify-between px-4">
           <div className="border-l border-white/20" />
           <div className="hidden border-l border-dashed border-white/20 md:block" />
           <div className="hidden border-l border-dashed border-white/20 lg:block" />
